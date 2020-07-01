cc21de14a1dfbbfc220f25c9a20d4491b92dfa44
kazudmb <nakano.kazuki.dmb@gmail.com>
2020-07-01 20:09:43 +0900

    add Project 11, part 1

--- Bookworm/ContentView.swift
+++ Bookworm/ContentView.swift
@@ -9,10 +9,80 @@
 import SwiftUI
 
 struct ContentView: View {
+//    @State private var rememberMe = false
+//
+//    var body: some View {
+//        VStack {
+//            PushButton(title: "Remember Me", isOn: $rememberMe)
+//            Text(rememberMe ? "On" : "Off")
+//        }
+//    }
+    
+//    @Environment(\.horizontalSizeClass) var sizeClass
+//
+//    var body: some View {
+//        if sizeClass == .compact {
+//            return AnyView(VStack {
+//                Text("Active size class:")
+//                Text("COMPACT")
+//            }
+//            .font(.largeTitle))
+//        } else {
+//            return AnyView(HStack {
+//                Text("Active size class:")
+//                Text("REGULAR")
+//            }
+//            .font(.largeTitle))
+//        }
+//    }
+    
+    @Environment(\.managedObjectContext) var moc
+    @FetchRequest(entity: Student.entity(), sortDescriptors: []) var students: FetchedResults<Student>
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            List {
+                ForEach(students, id: \.id) { student in
+                    Text(student.name ?? "Unknown")
+                }
+            }
+            
+            Button("Add") {
+                let firstNames = ["Ginny", "Harry", "Hermione", "Luna", "Ron"]
+                let lastNames = ["Granger", "Lovegood", "Potter", "Weasley"]
+
+                let chosenFirstName = firstNames.randomElement()!
+                let chosenLastName = lastNames.randomElement()!
+
+                let student = Student(context: self.moc)
+                student.id = UUID()
+                student.name = "\(chosenFirstName) \(chosenLastName)"
+                
+                try? self.moc.save()
+            }
+        }
     }
 }
+
+struct PushButton: View {
+    let title: String
+    @Binding var isOn: Bool
+
+    var onColors = [Color.red, Color.yellow]
+    var offColors = [Color(white: 0.6), Color(white: 0.4)]
+
+    var body: some View {
+        Button(title) {
+            self.isOn.toggle()
+        }
+        .padding()
+        .background(LinearGradient(gradient: Gradient(colors: isOn ? onColors : offColors), startPoint: .top, endPoint: .bottom))
+        .foregroundColor(.white)
+        .clipShape(Capsule())
+        .shadow(radius: isOn ? 0 : 5)
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
