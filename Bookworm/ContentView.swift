a806b7e2382f91776679d0b7dff9768fcb719eec
kazudmb <nakano.kazuki.dmb@gmail.com>
2020-07-02 09:02:06 +0900

    add Project 11, part 2

--- Bookworm/ContentView.swift
+++ Bookworm/ContentView.swift
@@ -9,80 +9,40 @@
 import SwiftUI
 
 struct ContentView: View {
-//    @State private var rememberMe = false
-//
-//    var body: some View {
-//        VStack {
-//            PushButton(title: "Remember Me", isOn: $rememberMe)
-//            Text(rememberMe ? "On" : "Off")
-//        }
-//    }
-    
-//    @Environment(\.horizontalSizeClass) var sizeClass
-//
-//    var body: some View {
-//        if sizeClass == .compact {
-//            return AnyView(VStack {
-//                Text("Active size class:")
-//                Text("COMPACT")
-//            }
-//            .font(.largeTitle))
-//        } else {
-//            return AnyView(HStack {
-//                Text("Active size class:")
-//                Text("REGULAR")
-//            }
-//            .font(.largeTitle))
-//        }
-//    }
-    
     @Environment(\.managedObjectContext) var moc
-    @FetchRequest(entity: Student.entity(), sortDescriptors: []) var students: FetchedResults<Student>
+    @FetchRequest(entity: Book.entity(), sortDescriptors: []) var books: FetchedResults<Book>
+    
+    @State private var showingAddScreen = false
     
     var body: some View {
-        VStack {
+        NavigationView {
             List {
-                ForEach(students, id: \.id) { student in
-                    Text(student.name ?? "Unknown")
+                ForEach(books, id: \.self) { book in
+                    NavigationLink(destination: Text(book.title ?? "Unknown Title")) {
+                        EmojiRatingView(rating: book.rating)
+                            .font(.largeTitle)
+                        
+                        VStack(alignment: .leading) {
+                            Text(book.title ?? "Unknown Title")
+                                .font(.headline)
+                            Text(book.author ?? "Unknown Author")
+                                .foregroundColor(.secondary)
+                        }
+                    }
                 }
             }
-            
-            Button("Add") {
-                let firstNames = ["Ginny", "Harry", "Hermione", "Luna", "Ron"]
-                let lastNames = ["Granger", "Lovegood", "Potter", "Weasley"]
-
-                let chosenFirstName = firstNames.randomElement()!
-                let chosenLastName = lastNames.randomElement()!
-
-                let student = Student(context: self.moc)
-                student.id = UUID()
-                student.name = "\(chosenFirstName) \(chosenLastName)"
-                
-                try? self.moc.save()
+            .navigationBarTitle("Bookworm")
+            .navigationBarItems(trailing: Button(action: {
+                self.showingAddScreen.toggle()
+            }) {
+                Image(systemName: "plus")
+            })
+                .sheet(isPresented: $showingAddScreen) {
+                    AddBookView().environment(\.managedObjectContext, self.moc)
             }
         }
     }
 }
-
-struct PushButton: View {
-    let title: String
-    @Binding var isOn: Bool
-
-    var onColors = [Color.red, Color.yellow]
-    var offColors = [Color(white: 0.6), Color(white: 0.4)]
-
-    var body: some View {
-        Button(title) {
-            self.isOn.toggle()
-        }
-        .padding()
-        .background(LinearGradient(gradient: Gradient(colors: isOn ? onColors : offColors), startPoint: .top, endPoint: .bottom))
-        .foregroundColor(.white)
-        .clipShape(Capsule())
-        .shadow(radius: isOn ? 0 : 5)
-    }
-}
-
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
